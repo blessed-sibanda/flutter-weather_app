1614894e231f2dd00625e28e5d96dda926339e71
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-28 09:37:54 +0200

    implement color-transition-box widget

---
+++ lib/widgets/color_transition_box.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ColorTransitionBox extends AnimatedWidget {
+  final Animation<Color> animation;
+  final Widget child;
+
+  const ColorTransitionBox({
+    Key? key,
+    required this.child,
+    required this.animation,
+  }) : super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: animation.value,
+      ),
+      child: child,
+    );
+  }
+}
